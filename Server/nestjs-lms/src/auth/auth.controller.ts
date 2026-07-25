fd25068d51c7c7be691ada925835e3a6fb49ed0b
Vikas Pandey <vikeepandit178@gmail.com>
2026-07-25 15:30:32 +0530

    Fix nestjs-lms folder - add as regular files not submodule

---
+++ Server/nestjs-lms/src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/registerUser.dto';
+
+@Controller('auth')
+export class AuthController {
+   
+    constructor( private readonly  authService: AuthService) {  }
+    @Post('register')
+
+    register(@Body() registerUserDto:RegisterUserDto ){
+      const result = this.authService.registerUser(registerUserDto);
+        return result;
+       
+    }
+}
---
+++ Server/nestjs-lms/src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { RegisterUserDto } from './dto/registerUser.dto';
+import bcrypt from 'bcrypt';
+@Injectable()
+export class AuthService {
+    constructor(private readonly userService: UserService) {}
+  async registerUser(registerDto: RegisterUserDto){
+        console.log('Registering user:', registerDto);
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(registerDto.password, saltRounds);
+        registerDto.password = hashedPassword;
+      
+        //logic
+        /**
+         * - check email already exixt or not
+         * - hash password
+         * - save user to database
+         * - generate jwt token
+         * - return jwt token
+         *  - send token in response
+         */
+       const result = this.userService.createUser({...registerDto, password: hashedPassword});
+       return result;
+        // return {message: 'User registered successfully'};
+    }
+}
