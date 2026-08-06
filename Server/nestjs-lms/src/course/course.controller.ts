75359457ec7036f7f28d309c16de80921ec5aaca
Vikas Pandey <vikeepandit178@gmail.com>
2026-08-06 17:48:19 +0530

    "fetal: Authroization complete roles authoentication and custom decorator"

--- Server/nestjs-lms/src/course/course.controller.ts
+++ Server/nestjs-lms/src/course/course.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, HttpStatus, UseGuards } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/user/user.types';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('courses')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
   @Post()
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   create(@Body() createCourseDto: CreateCourseDto) {
     return this.courseService.create(createCourseDto);
   }
@@ -18,16 +24,20 @@ export class CourseController {
   }
 
   @Get(':id')
-findOne(@Param('id') id: string) {
-  return this.courseService.findOne(id);
-}
+  findOne(@Param('id') id: string) {
+    return this.courseService.findOne(id);
+  }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
+  @Roles(Role.Admin)
   update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
     return this.courseService.update(id, updateCourseDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   remove(@Param('id') id: string) {
     return this.courseService.remove(id);
   }
